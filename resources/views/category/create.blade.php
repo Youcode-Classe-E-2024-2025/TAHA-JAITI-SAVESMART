81e6edfc0e7fff50648c626fc1ef0310aa5ca8b6
taha jaiti <kyojin.rar@gmail.com>
2025-02-27 09:39:22 +0000

    RESET: redoing the whole project because it broke

--- resources/views/category/create.blade.php
+++
@@ -1,7 +0,0 @@
-<form action="{{ route('category.store') }}" method="POST">
-    @csrf
-
-    <input type="text" name="name">
-
-    <button type="submit">Create</button>
-</form>
--- routes/category.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\CategoryController;
-
-Route::controller(CategoryController::class)->group(function () {
-    Route::get('/category/create', 'create')->name('category.create');
-    Route::post('/category/create', 'store')->name('category.store');
-});
