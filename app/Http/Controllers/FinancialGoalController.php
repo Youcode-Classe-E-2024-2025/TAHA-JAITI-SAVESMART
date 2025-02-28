750638c80b3a4331a51f102e10b4f65601bcf6b7
taha jaiti <kyojin.rar@gmail.com>
2025-02-28 17:04:15 +0000

    FIX: fixed an issue where the goal index would show other users goal alongside the signed in user, by using query

--- app/Http/Controllers/FinancialGoalController.php
+++ app/Http/Controllers/FinancialGoalController.php
@@ -8,17 +8,31 @@ use Illuminate\Support\Facades\Auth;
 
 class FinancialGoalController extends Controller
 {
-    public function index(){
-        $financialGoals = FinancialGoal::paginate(10);
+    public function index()
+    {
+        $user = Auth::user();
+
+        $query = FinancialGoal::query();
+
+        if ($user->family_id) {
+            $query->where('family_id', $user->family_id);
+            $query->orWhere('user_id', $user->family_id);
+        } else {
+            $query->where('user_id', $user->id);
+        }
+
+        $financialGoals =$query->orderBy('created_at','desc')->paginate(10);
 
         return view('goal.index', compact('financialGoals'));
     }
 
-    public function create(){
+    public function create()
+    {
         return view('goal.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $request->validate([
             'name' => 'required|string|min:3|max:255',
@@ -35,7 +49,7 @@ class FinancialGoalController extends Controller
 
         $goal = FinancialGoal::create([
             'name' => $request->name,
-            'description'=> $request->description,
+            'description' => $request->description,
             'target' => $request->target,
             'current_amount' => $request->current_amount,
             'deadline' => $request->deadline,
@@ -49,22 +63,24 @@ class FinancialGoalController extends Controller
             return to_route('goal.index')->with('success', 'Goal created successfully');
         }
 
-        return to_route('goal.index')->with('error','Failed to create financial goal');
+        return to_route('goal.index')->with('error', 'Failed to create financial goal');
     }
 
-    public function show(FinancialGoal $goal){
+    public function show(FinancialGoal $goal)
+    {
         return view('goal.show', compact('goal'));
     }
 
-    public function destroy(FinancialGoal $goal){
+    public function destroy(FinancialGoal $goal)
+    {
         $user = Auth::user();
 
-        if ($goal->user_id != $user->id){
-            return back()->with('error','You are not authorized to delete this goal');
+        if ($goal->user_id != $user->id) {
+            return back()->with('error', 'You are not authorized to delete this goal');
         }
 
         $goal->delete();
 
-        return to_route('goal.index')->with('success','Goal deleted successfully');
+        return to_route('goal.index')->with('success', 'Goal deleted successfully');
     }
 }
